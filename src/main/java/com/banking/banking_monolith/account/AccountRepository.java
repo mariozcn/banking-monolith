b53e66205869c283d823e2d6e29682a0814463dd
mariozcn <rusumario2411@gmail.com>
2026-05-20 21:48:11 +0300

    Add Transaction entity

--- src/main/java/com/banking/banking_monolith/account/AccountRepository.java
+++ src/main/java/com/banking/banking_monolith/account/AccountRepository.java
@@ -1,6 +1,5 @@
 package com.banking.banking_monolith.account;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
---
+++ src/main/java/com/banking/banking_monolith/transaction/TransactionRequest.java
@@ -0,0 +1,8 @@
+package com.banking.banking_monolith.transaction;
+
+
+import org.springframework.stereotype.Service;
+
+public record TransactionRequest(
+){}
+
